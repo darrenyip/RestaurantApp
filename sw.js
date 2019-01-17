6960aa0d97ca244a461d21ae861c7666d12bad8d
darrenyip <darrenyipzy@gmail.com>
2019-01-16 21:34:47 -0500

    service worker not fully working yet

--- sw.js
+++ sw.js
@@ -51,25 +51,42 @@ self.addEventListener('activate', function(event){
 })
 
 //Call fetch
+// self.addEventListener('fetch', function(event) {
+//     console.log('Handling fetch event for', event.request.url);
+//     event.respondWith(
+//         caches.match(event.request).then(function(response) {
+//         if (response) {
+//             console.log('Found response in cache:', response);
+//             return response;
+//         }
+//         console.log('No response found in cache. About to fetch from network...');
+        
+//         return fetch(event.request).then(function(response) {
+//             console.log('Response from network is:', response);
+            
+//             return response;
+//         }).catch(function(error) {
+//             console.error('Fetching failed:', error);
+
+//             throw error;
+//             });
+//         })
+//     );
+// });
+
 self.addEventListener('fetch', function(event) {
     console.log('Handling fetch event for', event.request.url);
     event.respondWith(
-        caches.match(event.request).then(function(response) {
-        if (response) {
-            console.log('Found response in cache:', response);
-            return response;
-        }
-        console.log('No response found in cache. About to fetch from network...');
-        
-        return fetch(event.request).then(function(response) {
-            console.log('Response from network is:', response);
-            
-            return response;
-        }).catch(function(error) {
-            console.error('Fetching failed:', error);
-
-            throw error;
-            });
+        caches.match(event.request).then(function(response){
+            if(response){
+                console.log('Found response in cache:', response);
+                return response;
+            }
+            else{
+                console.log('Response from network is:', response);
+                return fetch(event.request);
+            }
         })
     );
 });
+
