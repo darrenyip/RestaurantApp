0f47a8749b023f8515eed28d5108aab6a1ab2214
darrenyip <darrenyipzy@gmail.com>
2019-01-16 22:26:22 -0500

    final final final fix on service worker

--- sw.js
+++ sw.js
@@ -1,23 +1,22 @@
-const name = "restaurantApp";
-
 const cacheData = [
-    'index.html',
-    'restaurant.html',
-    'css/styles.css',
-    'data/restaurants.json',
-    'img/1.jpg',
-    'img/2.jpg',
-    'img/3.jpg',
-    'img/4.jpg',
-    'img/5.jpg',
-    'img/6.jpg',
-    'img/7.jpg',
-    'img/8.jpg',
-    'img/9.jpg',
-    'img/10.jpg',
-    'js/dbhelper.js',
-    'js/main.js',
-    'js/restaurant_info.js'
+    '/',
+    '/index.html',
+    '/restaurant.html',
+    '/css/styles.css',
+    '/data/restaurants.json',
+    '/img/1.jpg',
+    '/img/2.jpg',
+    '/img/3.jpg',
+    '/img/4.jpg',
+    '/img/5.jpg',
+    '/img/6.jpg',
+    '/img/7.jpg',
+    '/img/8.jpg',
+    '/img/9.jpg',
+    '/img/10.jpg',
+    '/js/dbhelper.js',
+    '/js/main.js',
+    '/js/restaurant_info.js'
 ];
 
 //call install
@@ -25,68 +24,70 @@ self.addEventListener('install',event =>{
     console.log("SW installing...");
     event.waitUntil(
         caches
-            .open(name)
+            .open('v1')
             .then(cache =>{
                 console.log('SW: caching...');
-                cache.addAll(cacheData);
+                return cache.addAll(cacheData);
             })
             .then(()=>self.skipWaiting())
     )
 })
 
 //call activate
-self.addEventListener('activate', function(event){
-    var cacheKeepList = ['v2'];
-    console.log("SW: activated")
-    event.waitUntil(
-        caches.keys().then(function(keyList){
-            return Promise.all(keyList.map(function(key){
-                if(cacheKeepList.indexOf(key) === -1){
-                    console.log("SW: deleting old cache");
-                    return caches.delete(key);
-                }
-            }))
-        })
-    )
-})
+// self.addEventListener('activate', function(event){
+//     var cacheKeepList = ['v2'];
+//     console.log("SW: activated")
+//     event.waitUntil(
+//         caches.keys().then(function(keyList){
+//             return Promise.all(keyList.map(function(key){
+//                 if(cacheKeepList.indexOf(key) === -1){
+//                     console.log("SW: deleting old cache");
+//                     return caches.delete(key);
+//                 }
+//             }))
+//         })
+//     )
+// })
 
 //Call fetch
-// self.addEventListener('fetch', function(event) {
-//     console.log('Handling fetch event for', event.request.url);
-//     event.respondWith(
-//         caches.match(event.request).then(function(response) {
-//         if (response) {
-//             console.log('Found response in cache:', response);
-//             return response;
-//         }
-//         console.log('No response found in cache. About to fetch from network...');
-        
-//         return fetch(event.request).then(function(response) {
-//             console.log('Response from network is:', response);
-            
-//             return response;
-//         }).catch(function(error) {
-//             console.error('Fetching failed:', error);
-
-//             throw error;
-//             });
-//         })
-//     );
-// });
-
 self.addEventListener('fetch', function(event) {
     console.log('Handling fetch event for', event.request.url);
     event.respondWith(
-        caches.match(event.request).then(function(response){
-            if(response){
-                console.log('Found response in cache:', response);
-                return response;
-            }
-            else{
-                console.log('Response from network is:', response);
-                return fetch(event.request);
-            }
+        caches.match(event.request).then(function(response) {
+        if (response) {
+            console.log('Found response in cache:', response);
+            return response;
+        }
+        console.log('No response found in cache. About to fetch from network...');
+        
+        return fetch(event.request).then(function(response) {
+            console.log('Response from network is:', response);
+            
+            return response;
+        }).catch(function(error) {
+            console.error('Fetching failed:', error);
+
+            throw error;
+            });
         })
     );
 });
+
+
+
+// self.addEventListener('fetch', function(event) {
+//     console.log('Handling fetch event for', event.request.url);
+//     event.respondWith(
+//         caches.match(event.request).then(function(response){
+//             if(response){
+//                 console.log('Found response in cache:', response);
+//                 return response;
+//             }
+//             else{
+//                 console.log('Response from network is:', response);
+//                 return fetch(event.request);
+//             }
+//         })
+//     );
+// });
 
